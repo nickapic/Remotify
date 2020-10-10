81d974ddbe882c84c04d82a28154f1a74ce41956
nickapic <43927184+nickapic@users.noreply.github.com>
2020-10-11 00:17:11 +0300

    My Collegue Issue Fixed

--- src/page/mycolleagues.jsx
+++ src/page/mycolleagues.jsx
@@ -2,28 +2,23 @@ import React, { useState } from 'react'
 import { Menu } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
 import { Category } from '../components/categorymenu';
-import { Profile } from '../components/search';
 export const MyColleagues = () => {
     const [current, setCurrent] = useState("followers");
     const handleClick = e => {
-        console.log('click ', e.key);
+        console.log('click ', e);
         setCurrent(e.key);
       };
     return (
         <div className="colleague-section"> 
         <Category />
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" className="menu">
-        <Menu.Item key="followers" icon={<MailOutlined />} className="menu-item">
+         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" className="menu">
+        <Menu.Item key="Followers" icon={<MailOutlined />} className="menu-item">
           Followers
         </Menu.Item>
         <Menu.Item key="following" icon={<MailOutlined />} className="menu-item">
           Following
         </Menu.Item>
-        <Menu.Item key="updates" icon={<MailOutlined />} className="menu-item">
-          Updates
-        </Menu.Item>
-        </Menu>
-        <Profile/>
-        </div>
+      </Menu>
+    </div>
     )
 }
