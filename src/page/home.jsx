fc2e9e87d65bd87a358f0dbabffa220fb0049e58
nickapic <43927184+nickapic@users.noreply.github.com>
2020-10-10 15:58:59 +0300

    Initial Design

---
+++ src/page/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Dashboard } from '../components/dashboard'
+
+export const HomePage = () => {
+    return (
+        <div>
+            <Dashboard/>
+        </div>
+    )
+}
