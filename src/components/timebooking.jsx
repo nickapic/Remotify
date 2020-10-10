6133c846b2c261acec9c4460b6ebc310e541416c
nickapic <43927184+nickapic@users.noreply.github.com>
2020-10-10 19:26:28 +0300

    Location Side Implemented

--- src/components/timebooking.jsx
+++ src/components/timebooking.jsx
@@ -3,28 +3,51 @@ import { Calendar } from 'antd';
 import 'antd/dist/antd.css';
 import { TimePicker } from 'antd';
 import { Menu, Dropdown, Button } from 'antd';
+import { ReactComponent as MapSVG } from "../map.svg";
+import { Checkbox } from 'antd';
+import { ModalPop } from './modal_toinvite';
+
 
 const menu = (
     <Menu>
       <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        <a target="_blank" rel="noopener noreferrer" >
           Uniq
         </a>
       </Menu.Item>
       <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        <a target="_blank" rel="noopener noreferrer" >
           DC PIER
         </a>
       </Menu.Item>
       <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        <a target="_blank" rel="noopener noreferrer">
           DC Valley
         </a>
       </Menu.Item>
     </Menu>
   );
 
-
+  
+  const floors = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" >
+          1
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" >
+          2
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer">
+          3
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
 export const TimeBooking = () => {
     const { RangePicker } = TimePicker;
     const format = 'HH:mm';
@@ -42,10 +65,23 @@ export const TimeBooking = () => {
             </div>
         </div>
         <div className="time-booker-left">
-            <Dropdown overlay={menu} placement="bottomCenter">
-                <Button>Select the Building </Button>
+        <div className="text-headline">What Location would you like to Book?</div>
+        <div className="location-dropmenu">
+            <Dropdown overlay={menu} placement="bottomCenter" className="btn">
+                <Button >Select the Building </Button>
+            </Dropdown>
+            <Dropdown overlay={floors} placement="bottomCenter" className="btn">
+                <Button >Floor</Button>
             </Dropdown>
         </div>
+            <MapSVG width="120%" height="auto"/>
+            <div className="checkboxes-section">
+                <Checkbox> Invite My Team</Checkbox>
+                <ModalPop />
+            </div>
+            
+        </div>
+        
     </div>
        
     )
