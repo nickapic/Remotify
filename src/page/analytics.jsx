ea79d7f4ab96c14d964ec745f21b7972a3163014
Agota <45971426+agosu@users.noreply.github.com>
2020-10-10 23:09:44 +0300

    stats image

--- src/page/analytics.jsx
+++ src/page/analytics.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Category } from '../components/categorymenu'
+import stats from './stats.jpg'
 
 export class AnalyticsPage extends React.Component {
     
@@ -9,6 +10,7 @@ export class AnalyticsPage extends React.Component {
             <div>
                 <Category />
                 <div>ANALYTICS</div>
+                <img height="500px" width="800px" src={ stats} alt="stats" />
             </div>
         )
     }
