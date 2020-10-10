2dc7547cf373be6f0c5b6fafb32b1d737964f9c7
nickapic <43927184+nickapic@users.noreply.github.com>
2020-10-10 22:56:07 +0300

    Calender Page Implemented

--- src/page/calender.jsx
+++ src/page/calender.jsx
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react'
 import { Category } from '../components/categorymenu'
 import { ReactComponent as CalendarSVG } from "./calendar.svg";
+import { ReactComponent as LegendSVG } from "./Legend.svg";
 export const CalenderPage = () => {
     return (
         
         <Fragment>
         <Category />
         <div className="calender-mine">
-            <CalendarSVG/>
+            <CalendarSVG className="calender_svg"/>
+            <LegendSVG className="legend_svg"/>
         </div>
 
         </Fragment>
